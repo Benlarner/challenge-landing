5db53e7ac8be3d6db67f60fce566dbd94574aa5e
Benlarner <stroudwoodfitness@gmail.com>
2020-09-05 23:26:52 -0400

    brigness dimmed on modal trigger

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 let modal = document.getElementById('modal');
 let button = document.getElementById('modal-button');
 let escape = document.getElementById('escape');
+let hire = document.getElementById('hire');
+let main = document.getElementById('main');
 
 function trigModal(){
     modal.style.display = 'unset';
     modal.style.display = 'flex';
+    main.style.filter = 'brightness(50%)';
 }
 
 function unTrigModal(){
     modal.style.display = "unset";
     modal.style.display = "none";
+    main.style.filter = 'unset';
 }
+
+
 
 button.addEventListener('click', trigModal);
 escape.addEventListener('click', unTrigModal);
